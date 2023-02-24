5532bba642c73bd4709d1a2c1f342e4b74800711
samudraon <samudraon@gmailcom>
2023-02-25 01:00:20 +0600

    countries api added

--- js/randomUser.js
+++ js/randomUser.js
@@ -7,9 +7,16 @@ const loadUser = () => {
 const displayUser = user => {
     const nameTag = document.getElementById('name');
     nameTag.innerHTML = user.results[0].name.title + ' ' + user.results[0].name.first + ' ' + user.results[0].name.last;
+
     const genderTag = document.getElementById('gender');
     genderTag.innerHTML = user.results[0].gender;
-    console.log(user.results[0].name);
+
+    const locationTag = document.getElementById('location');
+    locationTag.innerHTML = user.results[0].location.street.number + ', ' + user.results[0].location.street.name + user.results[0].location.city+ ', ' + user.results[0].location.country;
+
+    const imageTag = document.getElementById('image');
+    imageTag.innerHTML = user.results[0].picture.large;
+    console.log(user.results[0].location.city);
 }
 
 loadUser();
