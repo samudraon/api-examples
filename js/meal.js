8ce8a4ccb579ab372749635149ac82aa9eadbfe3
samudraon <samudraon@gmailcom>
2023-02-25 21:51:20 +0600

    modal added

---
+++ js/meal.js
@@ -0,0 +1,53 @@
+const loadMeals = (searchText) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMeals(data.meals))
+}
+
+const displayMeals = meals => {
+    console.log(meals);
+    const mealsContainer = document.getElementById('meals-container');
+    mealsContainer.innerText = '';
+    meals.forEach(meal => {
+        const mealDiv = document.createElement('div');
+        mealDiv.classList.add('col');
+        mealDiv.innerHTML = `
+        <div class="card h-100">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.</p>
+                <button onclick="loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetailsModal">
+                    Details
+                </button>
+            </div>
+        </div>
+        `
+        mealsContainer.appendChild(mealDiv);
+    })
+}
+
+const searchMeals = () => {
+    const searchField = document.getElementById('search-field').value;
+    loadMeals(searchField);
+}
+
+const loadMealDetail = idMeal => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+    // console.log(idMeal);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]));
+}
+
+const displayMealDetails = meal => {
+    document.getElementById('mealDetailsModalLabel').innerHTML = meal.strMeal;
+    const mealDetails = document.getElementById('mealDetailsBody');
+    mealDetails.innerHTML = `
+    <img class="img-fluid" src="${meal.strMealThumb}">
+    `
+}
+
+loadMeals('burger');
